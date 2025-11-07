3704dffc7626faf062feedeeb8b882aeb4138550
Pkrasytskyi <pkrasytskyi@gmail.com>
2025-11-07 13:04:23 +0100

    chore: update maven.yml, pom.xml

--- src/test/java/tests/SaucedemoInventoryPageTests.java
+++ src/test/java/tests/SaucedemoInventoryPageTests.java
@@ -101,10 +101,11 @@ public class SaucedemoInventoryPageTests extends BaseTest {
 
         String itemToRemove = items[0];
         Allure.step("Remove item: " + itemToRemove);
+        Thread.sleep(5000);
         cartPage.clickRemoveButton(itemToRemove);
         Thread.sleep(5000);
         List<String> afterRemove = cartPage.getItemNameFromCart();
-
+        Thread.sleep(5000);
         Allure.step("Verify removed item is not in cart and others remain");
         Assert.assertEquals(afterRemove.size(), beforeRemove.size() - 1, "Item count did not decrease by 1");
         Assert.assertFalse(afterRemove.contains(itemToRemove), "Removed item is still in the cart");
